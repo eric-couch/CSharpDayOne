d84b99875a23983b8120ae944226641e88d13a85
Eric Couch <james.couch@ruminatetech.com>
2019-11-26 21:55:13 -0600

    Add project files.

---
+++ HelloWorld/Dog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Dog
+    {
+        String Breed { get; set; }
+        String Name { get; set; }
+        float Weight { get; set; }
+
+        public String Bark()
+        {
+            return "woof";
+        }
+
+        public Dog(string breed, string name, float weight)
+        {
+            Breed = breed;
+            Name = name;
+            Weight = weight;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} is a {1} and weighs {2} lbs.", Name, Breed, Weight);
+        }
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog Joker = new Dog("Malamute", "Joker", 110.0f);
+            Console.WriteLine(Joker.ToString());
+        }
+    }
+}
